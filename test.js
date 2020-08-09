323ccad6f5ecc91cdd6089b93bac268479fd6689
lyj1023 <1778031084@qq.com>
2020-08-09 22:28:50 +0800

    测试node-schdule定时器功能

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
-var aaa = "name";
-console.log(aaa);
+var schedule = require("node-schedule");  
+
+var rule1     = new schedule.RecurrenceRule();  
+
+var times1    = [1,11,21,31,41,51];  
+
+rule1.hour  = times1;  
+
+schedule.scheduleJob('1-3 * * * * *', function(){  
+
+  console.log(new Date());
+
+});  
